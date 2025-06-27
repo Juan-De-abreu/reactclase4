8adc16191696b1ada640920e3c2581fed8011b22
jesus <jesusjsgdev@gmail.com>
2025-06-27 16:23:44 -0400

    hol

--- pages/Tendencias.jsx
+++ pages/Tendencias.jsx
@@ -71,10 +71,30 @@ const Tendencias = () => {
 
                 <div className="card-footer text-center ">
                     <a href="#" className="btn btn-primary me-3" data-bs-toggle="modal" data-bs-target={`#${item.id}`}>Modal</a>
-                    <Link to={''} href="#" className='btn btn-primary me-3'>Detalles</Link>
+                    <a to={''} href="#" className='btn btn-primary me-3'>Detalles</a>
                 </div>
 
             </div>
+            
+
+
+<div class="modal fade" id={`${item.id}`} tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">{item.title}</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
         </div>
     ))}
   </div>
